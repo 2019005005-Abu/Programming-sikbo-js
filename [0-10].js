7cab4ad8dda30dce18239259c03f586239ebe7e9
Abu Al Shahriar Rifat <79824559+2019005005-Abu@users.noreply.github.com>
2025-07-08 05:27:04 +0600

    Add files via upload

---
+++ [0-10].js
@@ -0,0 +1,37 @@
+console.log("I am a full stack developer")
+console.warn("hi")
+//document.writeln("I am a full stack developer")
+//alert(" welcome to the Javascript world")
+//alert(10+23)
+console.log(100+35)
+let n1='1083';
+n1=parseInt(n1)
+let n2='53';
+n2=parseInt(n2);
+let res=n1+n2;
+console.log(res)
+console.log(Math.round(Math.log(23)))
+console.log(Math.round(Math.random(1,100)))
+console.log(Math.max(1029,464,746))
+console.log(Math.min(53,75))
+console.log(Math.abs(-5))
+console.log(Math.cos(100))
+console.log(Math.round(Math.cosh(54)))
+f=636.63535;
+console.log(Math.round(f%43))
+maxIs=73<646;
+if (maxIs){
+    console.log("Ok")
+}else{
+    console.log("Not Ok")
+}
+
+console.log(typeof 'Name')
+console.log(typeof 23)
+console.log(typeof true);
+console.log(typeof NaN)
+console.log(typeof null)
+console.log(typeof 34.56)
+console.log(!true)
+console.log(!false)
+
